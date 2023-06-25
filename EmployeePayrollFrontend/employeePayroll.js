f52233aede1fea82953ce714652053ff19ee3146
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-06-25 11:41:38 +0530

    [ seperate js file]

--- EmployeePayrollFrontend/employeePayroll.js
+++ EmployeePayrollFrontend/employeePayroll.js
@@ -6,8 +6,14 @@
          class EmployeePayroll {
 
             // getter and setter method
-            get id() { return this._id; }
-            set id(id) { this._id = id; }
+            get id() 
+            { 
+                return this._id; 
+            }
+            set id(id) 
+            { 
+                this._id = id; 
+            }
         
             get name() { return this._name; }
             set name(name) { this._name = name; }
